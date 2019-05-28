3b313e50f543683200c8d5fd03501c7b4dcb49d2
Valentyn Saik <s19319@pjwstk.edu.pl>
2019-05-28 15:08:25 +0200

    Idk just playing around...

--- src/puzzle/Main.java
+++ src/puzzle/Main.java
@@ -1,22 +1,29 @@
 package puzzle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements IController {
+    @FXML
+    AnchorPane root;
 
     @Override
     public void start(Stage primaryStage) {
+        IndexController indexController = new IndexController();
 
-        IndexController mainContainer = new IndexController();
-        mainContainer.loadScreen("name", "fxml file");
+        boolean isScreenLoaded = indexController.loadScreen("mainMenu.fxml");
 
-        mainContainer.setScreen("mainMenu.fxml");
+        if (isScreenLoaded == false) {
+            System.out.println("Hello!");
+            return;
+        }
 
         Group root = new Group();
-        root.getChildren().addAll(mainContainer);
+        root.getChildren().addAll(indexController.getScreen("mainMenu.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -24,5 +31,15 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @FXML
+    public void onNewGame() {
+        System.out.println("Hello222!");
+    }
+
+    @Override
+    public void setScreenParent(IController screenPage) {
+
     }
 }
