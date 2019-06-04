722dc872f8da0e16f421097cfc78472bcb83188a
Valentyn Saik <s19319@pjwstk.edu.pl>
2019-06-04 15:07:36 +0200

    Index controller refactoring

---
+++ src/puzzle/IndexController.java
@@ -0,0 +1,30 @@
+package puzzle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class IndexController implements Initializable, IController {
+    private ScreenChangerService screenChangerService;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    public void setScreenParent(ScreenChangerService screenParent){
+        screenChangerService = screenParent;
+    }
+
+    @FXML
+    private void newGame(ActionEvent event){
+       screenChangerService.setScreen("newGame.fxml");
+    }
+    
+    @FXML
+    private void showScoreboard(ActionEvent event){
+       screenChangerService.setScreen("scoreboard.fxml");
+    }
+}
