7a69fc53a6d204de5d260ca6d6ceb82476bbce5f
Valentyn Saik <svbackend22@gmail.com>
2019-06-08 09:02:56 +0200

    Parameters for starting new game

--- src/puzzle/GameParameterBag.java
+++ src/puzzle/GameParameterBag.java
@@ -2,7 +2,20 @@ package puzzle;
 
 import javafx.scene.image.Image;
 
-public interface IGameParameterBag extends IParameterBag {
-    Image getImage();
-    String getUsername();
+class GameParameterBag implements IParameterBag {
+    private final Image image;
+    private final String username;
+
+    GameParameterBag(Image image, String username) {
+        this.image = image;
+        this.username = username;
+    }
+
+    Image getImage() {
+        return image;
+    }
+
+    String getUsername() {
+        return username;
+    }
 }
