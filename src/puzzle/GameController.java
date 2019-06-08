87a7b99a900ea0305cb5bbf090a7648d89c31b99
Valentyn Saik <svbackend22@gmail.com>
2019-06-08 09:01:59 +0200

    Get data from parameter bag and display it

--- src/puzzle/GameController.java
+++ src/puzzle/GameController.java
@@ -3,29 +3,54 @@ package puzzle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable, IController {
-    ScreenChangerService myController;
+    ScreenChangerService screenChanger;
+
+    static String call = "1";
+
+    @FXML
+    Label usernameLabel;
+
+    @FXML
+    ImageView imageView;
+
+    Image userImage;
+    String username;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        usernameLabel.setText("Loading..." + call);
+        call = "2";
     }
     
-    public void setScreenChanger(ScreenChangerService screenParent){
-        myController = screenParent;
+    public void setScreenChanger(ScreenChangerService screenChanger){
+        this.screenChanger = screenChanger;
+    }
+
+    public void setParameterBag(IParameterBag parameterBag) {
+        this.userImage = ((GameParameterBag) parameterBag).getImage();
+        this.username = ((GameParameterBag) parameterBag).getUsername();
+
+        usernameLabel.setText(this.username);
+        imageView.setImage(this.userImage);
     }
 
     @FXML
     private void goToScreen1(ActionEvent event){
-       myController.setScreen("newGame.fxml");
+       screenChanger.setScreen("newGame.fxml");
     }
     
     @FXML
     private void goToScreen2(ActionEvent event){
-       myController.setScreen("scoreboard.fxml");
+       screenChanger.setScreen("scoreboard.fxml");
     }
 }
