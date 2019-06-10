ce7334322e8deeda214e50b22b6a88b0788cecb6
Valentyn Saik <svbackend22@gmail.com>
2019-06-10 09:09:35 +0200

    Remove thread for time calculation, use Date objects instead

--- src/puzzle/SpentTime.java
+++ src/puzzle/SpentTime.java
@@ -1,65 +1,28 @@
 package puzzle;
 
-import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class SpentTime {
-    private int seconds = 0;
-    private int minutes = 0;
+    private Date started;
+    private Date ended;
 
-    private Thread thread;
-
-    private boolean threadIsActive = true;
+    private SimpleDateFormat formatter;
 
     SpentTime() {
-        this.thread = new Thread(() -> {
-            SwingUtilities.invokeLater(() -> {
-                while (threadIsActive) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    this.addSecond();
-                }
-            });
-        });
-
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException e)  {
-            e.printStackTrace();
-        }
+        this.formatter = new SimpleDateFormat("mm:ss");
+        this.started = new Date(System.currentTimeMillis());
     }
 
     public void stop() {
-        thread.stop();
-        threadIsActive = false;
-    }
-
-    public void addSecond() {
-        this.seconds++;
-
-        if (seconds == 61) {
-            seconds = 1;
-            minutes++;
-        }
-    }
-
-    public int getMinutes() {
-        return this.minutes;
-    }
-
-    public int getSeconds() {
-        return this.seconds;
+        this.ended = new Date(System.currentTimeMillis());
     }
 
     public String toString() {
-        if (minutes > 0) {
-            return this.minutes + " min. " + this.seconds + "sec.";
+        if (ended == null) {
+            return formatter.format(new Date(System.currentTimeMillis() - started.getTime()));
         }
 
-        return this.seconds + "sec.";
+        return formatter.format(new Date(ended.getTime() - started.getTime()));
     }
 }
